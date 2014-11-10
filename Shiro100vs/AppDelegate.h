dfc1b48450501c4f23f3157ce685dcb7c4ea47b9
寺内 信夫 <tenobu@gmail.com>
2014-11-10 11:24:00 +0900

    Initial Commit

---
+++ Shiro100vs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shiro100vs
+//
+//  Created by 寺内 信夫 on 2014/11/10.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shiro100vsTests/Shiro100vsTests.m
@@ -0,0 +1,40 @@
+//
+//  Shiro100vsTests.m
+//  Shiro100vsTests
+//
+//  Created by 寺内 信夫 on 2014/11/10.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Shiro100vsTests : XCTestCase
+
+@end
+
+@implementation Shiro100vsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
